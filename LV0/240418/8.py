25eab4002de0f8b3aab766b032e6acde726a84a3
차민재 <cmjj0824@naver.com>
2024-04-19 00:30:38 +0900

    ADD: 4월 18일 LV0 10문제 풀이

---
+++ LV0/240418/8.py
@@ -0,0 +1,5 @@
+def solution(price):
+  if(100000<=price<300000) : price = price*0.95
+  elif(300000<=price<500000) : price = price*0.9
+  elif(500000<=price) : price = price*0.8
+  return int(price)

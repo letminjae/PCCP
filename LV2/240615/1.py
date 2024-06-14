ed6d18675f19d4f827611a74fb3584134448ca66
차민재 <cmjj0824@naver.com>
2024-06-15 00:02:31 +0900

    6월 15일 문제풀이 완료

--- LV2/240615/1.py
+++ LV2/240615/1.py
@@ -1,7 +1,13 @@
 # 이진 변환 반복하기
 
 def solution(s):
-    answer = []
-    return answer
+  cnt = 0
+  loop = 0
+  while int(s) > 1:
+    loop += 1
+    cnt += s.count('0')
+    s = s.replace('0','')
+    s = bin(len(s))[2:]
+  return [loop, cnt]
 
 print(solution("110010101001"))

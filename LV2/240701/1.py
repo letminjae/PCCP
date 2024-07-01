52128ac08908e1733157cd6e2254f882e8f4b6a6
차민재 <cmjj0824@naver.com>
2024-07-01 19:02:07 +0900

    7월 1일 문제풀이 완료

---
+++ LV2/240701/1.py
@@ -0,0 +1,9 @@
+# n^2 배열 자르기
+
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right+1):
+        x = i // n
+        y = i % n
+        answer.append(max(x,y)+1)
+    return answer

516fe466ca4fa874aa0ce61ab64d641efbe35a8a
차민재 <cmjj0824@naver.com>
2025-03-08 13:39:33 +0900

    3월 7일 문제 - 반례로 인해 오답처리, heapq를 통해 다시 문제 해결

--- LV3/250307/1.py
+++ LV3/250307/1.py
@@ -2,12 +2,15 @@
 
 from collections import defaultdict
 
-def solution(tickets):
+def solution1(tickets):
     ticket_dict = defaultdict(list)
     
     # default dict로 key : 출발지, value : 목적지 딕셔너리 생성
     for start, end in tickets:
         ticket_dict[start].append(end)
+
+    for key in ticket_dict:
+        ticket_dict[key].sort()
 
     answer = []
     path = ["ICN"] # 출발 지점에서 계속 어디로 가는지 저장하는 리스트
@@ -25,7 +28,29 @@ def solution(tickets):
     # 역순으로 들어오기에 역으로 출력
     return answer[::-1]
 
-    
-
-solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]])
+# solution1([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]])
 # ["ICN", "JFK", "HND", "IAD"]
+
+# 반례로 인해 우선순위 큐를 사용한 방법으로 다시 풀어보기
+from collections import defaultdict
+import heapq
+
+def solution(tickets):
+    ticket_dict = defaultdict(list)
+
+    # 그래프 생성 (우선순위 큐 사용) -> 자동으로 오름차순되어 push
+    for start, end in tickets:
+        heapq.heappush(ticket_dict[start], end)
+
+    path = []
+    
+    def dfs(node):
+        while ticket_dict[node]:  # 현재 공항에서 갈 수 있는 공항이 남아있다면
+            next_node = heapq.heappop(ticket_dict[node])  # 가장 작은 알파벳 먼저 방문
+            dfs(next_node)  # 깊이 탐색
+        path.append(node)  # dfs로 더 이상 갈 곳이 없을 때 현재 공항을 기록
+
+    dfs("ICN")
+    return path[::-1]  # 최종 경로는 역순으로 반환
+
+solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]])

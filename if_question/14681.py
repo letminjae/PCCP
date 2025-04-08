01e9e0154a3fa09fb265a255fe3fa05203822554
차민재 <cmjj0824@naver.com>
2025-04-08 21:02:52 +0900

    14681 풀이완료

---
+++ if_question/14681.py
@@ -0,0 +1,13 @@
+# 14681 사분면 고르기
+
+X = int(input())
+Y = int(input())
+
+if X > 0 and Y > 0:
+    print(1)
+elif X < 0 and Y > 0:
+    print(2)
+elif X < 0 and Y < 0:
+    print(3)
+elif X > 0 and Y < 0:
+    print(4)

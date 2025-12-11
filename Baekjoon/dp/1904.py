35127db06a84a9851a6b4ac87f5db8cc543e9890
차민재 <cmjj0824@naver.com>
2025-12-11 19:42:32 +0900

    12월 11일 문제풀이 완료

---
+++ Baekjoon/dp/1904.py
@@ -0,0 +1,15 @@
+# 1904 01타일
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * 10000001
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, N+1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
+
+print(dp[N])

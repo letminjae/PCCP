57420a20579d8340e60cf4a4c0b98e5ce4ff0926
차민재 <cmjj0824@naver.com>
2025-12-18 19:41:28 +0900

    12월 18일 문제풀이 완료

---
+++ Baekjoon/dp/11722.py
@@ -0,0 +1,16 @@
+# 11722 가장 긴 감소하는 부분 수열 - dp
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+dp = [1 for _ in range(N)]
+
+for i in range(1,N):
+    for j in range(i):
+        if arr[i] < arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))

7f24efec4bd5822416b1c61d7ca06cd43e490f50
차민재 <cmjj0824@naver.com>
2026-01-27 19:47:54 +0900

    1월 27일 문제풀이 완료

---
+++ Baekjoon/dp/11057.py
@@ -0,0 +1,13 @@
+# 11057 - 오르막 수 (실버 1)
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [1]*10
+    
+for i in range(N-1):
+    for j in range(1, 10):
+        dp[j] += dp[j-1]
+
+print(sum(dp) % 10007)
